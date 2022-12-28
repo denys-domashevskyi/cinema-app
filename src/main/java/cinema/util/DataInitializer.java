57746477c3a435362416c3c423398c19acd88b24
Denys Domashevskyi <denis.domashevskiy28@gmail.com>
2022-12-28 11:14:15 +0200

    Small fixes

--- src/main/java/cinema/util/DataInitializer.java
+++ src/main/java/cinema/util/DataInitializer.java
@@ -30,15 +30,15 @@ public class DataInitializer {
         Role userRole = new Role();
         userRole.setRoleName(USER);
         roleService.add(userRole);
-        User user1 = new User();
-        user1.setEmail("admin@i.ua");
-        user1.setPassword("admin123");
-        user1.setRoles(Set.of(adminRole));
-        userService.add(user1);
-        User user2 = new User();
-        user2.setEmail("user@i.ua");
-        user2.setPassword("user123");
-        user2.setRoles(Set.of(userRole));
-        userService.add(user2);
+        User admin = new User();
+        admin.setEmail("admin@i.ua");
+        admin.setPassword("admin123");
+        admin.setRoles(Set.of(adminRole));
+        userService.add(admin);
+        User user = new User();
+        user.setEmail("user@i.ua");
+        user.setPassword("user123");
+        user.setRoles(Set.of(userRole));
+        userService.add(user);
     }
 }
