95890d249fbdc48e5dee15749413bcc1adbe490b
Denys Domashevskyi <denis.domashevskiy28@gmail.com>
2022-12-28 11:18:04 +0200

    Small fixes

--- src/main/java/cinema/dao/impl/RoleDaoImpl.java
+++ src/main/java/cinema/dao/impl/RoleDaoImpl.java
@@ -19,7 +19,8 @@ public class RoleDaoImpl extends AbstractDao<Role> implements RoleDao {
     @Override
     public Optional<Role> getByName(String roleName) {
         try (Session session = factory.openSession()) {
-            Query<Role> query = session.createQuery("from Role r where r.roleName = :name", Role.class);
+            Query<Role> query = session.createQuery("from Role r where r.roleName = :name",
+                    Role.class);
             query.setParameter("name", Role.RoleName.valueOf(roleName));
             return query.uniqueResultOptional();
         } catch (Exception e) {
